0f972e867fa9fc8d025accb746341b65e4c62b91
mohamedkeshawy <mokeshawy@gmail.com>
2026-01-02 10:39:44 +0200

    create core module and add bases components such - bases app & viewmodel & repository - crash reporting & error handling & state & storage manager

---
+++ core/src/main/java/com/core/error/AppErrorException.kt
@@ -0,0 +1,29 @@
+package com.core.error
+
+import androidx.annotation.Keep
+
+
+open class AppErrorException(message: String? = null, cause: Throwable? = null) :
+    RuntimeException(message, cause)
+
+@Keep
+class GeneralException(message: String? = null, cause: Throwable? = null) :
+    AppErrorException(message, cause)
+
+@Keep
+class IoException(message: String? = null, cause: Throwable? = null) :
+    AppErrorException(message, cause)
+
+@Keep
+class ResponseException(message: String? = null, cause: Throwable? = null) :
+    AppErrorException(message, cause)
+
+@Keep
+class ResponseUnAuthorizedException(message: String? = null, cause: Throwable? = null) :
+    AppErrorException(message, cause)
+
+
+@Keep
+class BadRequestException(message: String? = null, cause: Throwable? = null) :
+    AppErrorException(message, cause)
+
