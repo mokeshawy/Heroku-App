8e28dac2c7b68f0a9f464edeb7c02425b669a3ea
mohamedkeshawy <mokeshawy@gmail.com>
2026-01-03 06:44:44 +0200

    navHost configuration & splash ui screen implementation

---
+++ app/src/main/java/com/heroku_app/features/splash_screen/graph/SplashGraph.kt
@@ -0,0 +1,19 @@
+package com.heroku_app.features.splash_screen.graph
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.heroku_app.features.splash_screen.presentation.SplashScreen
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data object SplashScreen
+
+fun NavGraphBuilder.splashGraph(navController: NavController) {
+    composable<SplashScreen> {
+        SplashScreen(onNavigateToOrderList = {
+            //TODO NAVIGATE TO HOME SCREEN
+        })
+    }
+}
